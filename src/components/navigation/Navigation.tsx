72f23fe0d6119b055ff66954e8033bdd15fd38b8
Foxvel <ivelmozhko6@gmail.com>
2023-04-20 17:55:58 +1000

    a

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { FC } from "react";
 import navItems from "./navigationData";
+import { useRouter } from "next/router";
 
 
 const Navigation: FC = () => {
+    const router = useRouter();
     return (
         <nav className='pt-10 flex justify-center'>
             <ul className='text-3xl flex space-x-10'>
@@ -12,7 +14,7 @@ const Navigation: FC = () => {
                         return (
                             <li key={navItem.text}>
                                 <Link
-                                    className="transition ease-in-out duration-500 hover:text-cyan-700"
+                                    className={`transition ease-in-out duration-500 hover:text-cyan-700 ${router.asPath == navItem.url ? 'text-blue-700' : null}`}
                                     href={`${navItem.url}`}
                                 >
                                     {navItem.text}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
+import { ChangeEvent, LegacyRef, createRef, useEffect, useRef, useState } from "react"
+
 export default function Home() {
+  const [ counter, setCounter ] = useState("");
+  
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if(ref.current) ref.current.innerText = new Date().toTimeString();
+  }, [counter]);
+
+  
+  const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setCounter(ev.target.value);
+    console.log(ev.target.value);
+  }
+
   return (
     <>
       <div className="h-screen">
         <div className="h-3/4 flex justify-center items-center tx-xl">Главная</div>
+        <div ref={ref}></div>
+        <input type="text" onChange={handleChange} value={counter} className="border-black justify-center flex"></input>
       </div>
     </>
   )
